453e597cf705895bfb8f3a68944acd78cdcc9e29
dingjingmaster <dingjing@live.cn>
2022-06-15 18:13:25 +0800

    小组件 card

--- src/pages/MainIndex.vue
+++ src/pages/MainIndex.vue
@@ -2,72 +2,53 @@
     <div id="main-index">
         <el-row :gutter="12">
             <el-col :span="6">
-                <el-card shadow="hover">
-
-                        <div class="card-header">
-                            <el-row>
-                                <el-col :span="8"><span>1300</span></el-col>
-                                <el-col :span="16">
-                                    <el-row>
-                                        <el-col :span="8"><small>金(Au)</small><br/><small> RMB/kg</small></el-col>
-                                        <el-col :span="16"><small> 更新时间:</small><br/><small>2022-06-10 17:20:31</small></el-col>
-                                    </el-row>
-                                </el-col>
-                            </el-row>
-                        </div>
-
-                        <el-table :data="tableData" style="width: 100%">
-                            <el-table-column prop="date" label="Date"/>
-                            <el-table-column prop="name" label="Name"/>
-                            <el-table-column prop="address" label="Address"/>
-                        </el-table>
-
-                </el-card>
+                <MyCard v-bind:price="price"
+                        v-bind:name="name"
+                        v-bind:unit="unit"
+                        v-bind:uTime="uTime"
+                        v-bind:detail="detail">
+                </MyCard>
             </el-col>
         </el-row>
     </div>
 </template>
 
 <script>
-    const tableData = [
-        {
-            date: '2016-05-03',
-            name: 'Tom',
-            address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-            date: '2016-05-02',
-            name: 'Tom',
-            address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-            date: '2016-05-04',
-            name: 'Tom',
-            address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-            date: '2016-05-01',
-            name: 'Tom',
-            address: 'No. 189, Grove St, Los Angeles',
-        },
-    ];
+    import MyCard from '@@/../plugins/LocalCard'
+
     export default {
-        name: "MainIndex.vue"
+        name: "MainIndex.vue",
+        data: function () {
+            return {
+                name: "金(Au)",
+                price: "1800",
+                unit: "RMB/kg",
+                uTime: "2022-06-10 17:20:31",
+                detail: [
+                    {
+                        area: '中国',
+                        price: '1000',
+                        unit: 'RMB/kg',
+                    },
+                    {
+                        area: '美国',
+                        price: '2000',
+                        unit: 'RMB/Kg',
+                    },
+                    {
+                        area: '伦敦',
+                        price: '3000',
+                        unit: 'RMB/kg',
+                    },
+                ]
+            }
+        },
+        components: {
+            MyCard
+        }
     }
 </script>
 
 <style>
-    .el-card {
-        height: 200px;
-    }
-    .card-header>.el-row>.el-col>span {
-        font-size: 39px;
-        font-weight: bold;
-        vertical-align: bottom;
-    }
-    .card-header>.el-row>.el-col>.el-row>.el-col small {
-        white-space:nowrap;
-        vertical-align: bottom;
-        horiz-align: center;
-    }
+
 </style>
---
+++ src/plugins/LocalCard.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="i-l-card">
+        <el-card shadow="hover">
+            <div id="i-l-card-header">
+                <el-row>
+                    <el-col :span="9"><span>{{ price }}</span></el-col>
+                    <el-col :span="15">
+                        <el-row>
+                            <el-col :span="8"><small>{{ name }}</small><br/><small>{{ unit }}</small></el-col>
+                            <el-col :span="16"><small> 更新时间:</small><br/><small>{{ uTime }}</small></el-col>
+                        </el-row>
+                    </el-col>
+                </el-row>
+            </div>
+            <el-table v-bind:data=detail style="width: 100%">
+                <el-table-column prop="area" label="区域"/>
+                <el-table-column prop="price" label="价格"/>
+                <el-table-column prop="unit" label="单位"/>
+            </el-table>
+        </el-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LocalCard",
+        props: {
+            name: String,
+            unit: String,
+            uTime: String,
+            price: String,
+            detail: Array
+        },
+    }
+</script>
+
+<style>
+    #i-l-card {
+        height: 245px;
+    }
+    #i-l-card>.el-card>.el-table>.el-table-column {
+        font-size: 1px;
+    }
+    .el-card {
+        height: 240px;
+        min-width: 360px;
+        max-width: 380px;
+    }
+    #i-l-card-header>.el-row>.el-col>span {
+        font-size: 39px;
+        font-weight: bold;
+        vertical-align: bottom;
+    }
+    #i-l-card-header>.el-row>.el-col>.el-row>.el-col small {
+        white-space:nowrap;
+        vertical-align: bottom;
+        horiz-align: center;
+    }
+</style>
